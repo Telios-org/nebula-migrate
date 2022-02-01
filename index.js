70880010a673103117ead4f9481a0e8f5f6c117b
hexadecibal <hex#github@telios.io>
2022-02-01 17:09:19 -0500

    Refactoring email collection

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = async ({ rootdir, drivePath, keyPair, encryptionKey }) => {
   // 1. Output all transactions (encrypted) from Autobee into a migration folder. If migration folder exists, run migration
   try {
     fs.mkdirSync(path.join(rootdir, drivePath, 'migrate'))
-  
+
     // Start old drive
     const drive = new Drive(path.join(rootdir, drivePath), null, {
       keyPair,
@@ -31,6 +31,7 @@ module.exports = async ({ rootdir, drivePath, keyPair, encryptionKey }) => {
       keyPair,
       encryptionKey,
       joinSwarm: false,
+      fullTextSearch: true,
       swarmOpts: {
         server: true,
         client: true
@@ -47,11 +48,6 @@ module.exports = async ({ rootdir, drivePath, keyPair, encryptionKey }) => {
     // Remove new cores so they can be replace. The overwrite option in Hypercore does not seem to work as expected which is why these need to be deleted.
     await copyCores(rootdir, drivePath, encryptionKey)
 
-    // 4. Run transasction scripts to fill new Hypercores
-    await newDrive.ready()
-    await populateCores(newDrive, rootdir, drivePath)
-    await newDrive.close()
-
     // 5. Rename directories and files
     const files = fs.readdirSync(path.join(rootdir, drivePath, '/Files'))
 
@@ -59,6 +55,12 @@ module.exports = async ({ rootdir, drivePath, keyPair, encryptionKey }) => {
       fs.renameSync(path.join(rootdir, drivePath, '/Files', file), path.join(rootdir, 'drive_new', '/Files', file))
     }
 
+    // 4. Run transasction scripts to fill new Hypercores
+    await newDrive.ready()
+    await populateCores(newDrive, rootdir, drivePath)
+    await newDrive.close()
+
+    
     fs.renameSync(path.join(rootdir, drivePath), path.join(rootdir, drivePath + '_old'))
     fs.renameSync(path.join(rootdir, 'drive_new'), path.join(rootdir, drivePath))
   } catch(err) {
@@ -143,21 +145,23 @@ async function copyCores(rootdir, drivePath, encryptionKey) {
     cores = fs.readdirSync(path.join(rootdir, drivePath, '/Database'))
 
     for(const core of cores) {
-      let feed = new Hypercore(path.join(rootdir, drivePath, '/Database/' + core), { encryptionKey })
-      
-      await feed.ready()
+      if(core.indexOf('.DS_Store') === -1) {
+        let feed = new Hypercore(path.join(rootdir, drivePath, '/Database/' + core), { encryptionKey })
+        
+        await feed.ready()
 
-      let keyPair = feed.core.header.signer
+        let keyPair = feed.core.header.signer
 
-      await feed.close()
+        await feed.close()
 
 
-      feed = new HypercoreNew(path.join(rootdir, 'drive_new', '/Database/' + core), { keyPair, encryptionKey })
+        feed = new HypercoreNew(path.join(rootdir, 'drive_new', '/Database/' + core), { keyPair, encryptionKey })
 
-      await feed.ready()
+        await feed.ready()
 
-      keyPair = feed.core.header.signer
-      await feed.close()
+        keyPair = feed.core.header.signer
+        await feed.close()
+      }
     }
 
   } catch(err) {
@@ -173,18 +177,43 @@ async function populateCores(drive, rootdir, drivePath) {
     const newBee = drive.database.bee
     const newMetadb = drive.database.metadb
     const newLocalB = drive._localHB
-
+    
     for (const sub in data.main.collections) {
       const items = data.main.collections[sub]
       const collection = await drive.db.collection(sub)
-
+      
       for(const item of items) {
         // Not needed anymore
         delete item.value.__sub
-        await collection.insert({ ...item.value })
-        await indexDoc(sub, collection)
+
+        if(sub === 'Email') {
+          let email = await getEmail(drive, item.value.path)
+          
+          email = {
+            emailId: email.emailId,
+            aliasId: email.aliasId,
+            folderId: email.folderId,
+            unread: email.unread,
+            subject: email.subject,
+            toJSON: email.toJSON,
+            fromJSON: email.fromJSON,
+            ccJSON: email.ccJSON,
+            bccJSON: email.bccJSON,
+            bodyAsText: email.bodyAsText,
+            attachments: email.attachments,
+            path: email.path
+          }
+
+          await collection.insert(email)
+          await indexDoc(sub, collection)
+        } else {
+          await collection.insert({ ...item.value })
+          await indexDoc(sub, collection)
+        }
       }
     }
+
+    
 
     for(const tx of data.main.tx) {
       await newBee.insert({ ...tx.value })
@@ -197,6 +226,7 @@ async function populateCores(drive, rootdir, drivePath) {
     for(const tx of data.local) {
       await newLocalB.put(tx.key, tx.value)
     }
+    
   } catch(err) {
     throw err
   }
@@ -211,4 +241,28 @@ async function indexDoc(name, collection) {
       await collection.ftsIndex(['name', 'email'])
       break
   }
+}
+
+async function getEmail(drive, path) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const stream = await drive.readFile(path)
+
+      let decrypted = ''
+
+      stream.on('data', chunk => {
+        decrypted += chunk.toString('utf-8')
+      })
+
+      stream.on('end', () => {
+        return resolve(JSON.parse(decrypted))
+      })
+
+      stream.on('error', (err) => {
+        return reject(err)
+      })
+    } catch(err) {
+      return reject(err)
+    }
+  })
 }
